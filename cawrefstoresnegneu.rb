ff1baaacf5cbfbf9a9afe6ec3320fc7165a736e4
catsmells <47220664+catsmells@users.noreply.github.com>
2022-12-28 00:03:38 -0500

    Create cawrefstoresnegneu.rb

---
+++ cawrefstoresnegneu.rb
@@ -0,0 +1,15 @@
+opn = 0
+opp = '0x0'
+execfunc = [
+  # reset = complete 0x0 drain, nuke means complete burn
+  {:burn => 'reset', :mint => 'reset'},
+  {:burn => 'utilend', :mint => 'utilstart'},
+  {:burn => 'nuke', :mint => 'compstore'},
+  {:store => 'reset', :popstore => 'reset'},
+  {:store => 'nuke', :popstore => 'nuke'},
+  {:store => 'cwdata', :popstore => 'remcwd'}
+]
+cwgl.r do |negexec|
+  next if negexec[:burn] == 'reset'
+  opp = opp.confirmtr
+end
