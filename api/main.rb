27250f7290161baf6f9463dd39c88630eb59b491
catsmells <47220664+catsmells@users.noreply.github.com>
2023-01-07 19:20:50 -0500

    Update main.rb

--- api/main.rb
+++ api/main.rb
@@ -9,18 +9,26 @@ $charfield
 $staking
 $network
 $governance
+Send = lambda { `send.to " "` }
+Request lambda { `request.from " "` }
+Transaction = Tx
+Transaction = Tx lambda { `tx.of " "` }
 # send
 Send.Tokensend = Send::Address
 Send.Tokensend = Send::Username 
-Send.Packet = Send::`ping`
+Send.Packet = Send::`tuup ping`
 # request
-
+Request.Tokenrequest = Request::Address
+Request.Tokenrequest = Request::Username
+Request.Tokenrequest = Request::`tuup ping`
 # user
-
+Resquest.Address = Request::Address
+Resquest.Username = Request::Username
 # transaction
-
+Tx.GetID = Transaction::ID
+Tx.GetInvolved = Transaction::Involved # get users involved in specified transaction
 # network
-
+IsNetworkLive = `tuup ping network`
 # staking
 
 # governance
@@ -30,5 +38,8 @@ Send.Packet = Send::`ping`
 # address
 
 # nft
-
+NFT.Address = NFT::Address
+NFT.Creator = NFT::Creator
+NFT.Publish = NFT::Publish
+NFT.Republish = NFT::Republish
 # post
